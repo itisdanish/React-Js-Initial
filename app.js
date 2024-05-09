c3081267332c90914e67996c5438edabb06fb117
itisdanish <65316644+itisdanish@users.noreply.github.com>
2024-05-10 01:06:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,41 @@
 const parrent = React.createElement(
-    'div',
-    {
-        id:'parent'
-    },
-        [React.createElement(
-            'div',
-            {
-                id:'child1'
-            },
-            [
-                React.createElement('h1',{id:'heading'},'h1 Child1 Updated...'),
-                React.createElement('h2',{id:'heading'},'h2 Child1 Updated...')
-            ]
-        ),
-        React.createElement(
-            'div',
-            { 
-                id:'child2'
-            },
-            [
-                React.createElement('h1',{id:'heading'},'h1 Child2 Updated...'),
-                React.createElement('h2',{id:'heading'},'h2 Child2 Updated...')
-            ]
-        )
-        ]
-)
-
-console.log(parrent)
-const heading = React.createElement(
-    'h1',
-    {
-        id:'heading',
-        className:'heading'
-    },
-    'Hello React'
+  "div",
+  {
+    id: "parent",
+  },
+  [
+    React.createElement(
+      "div",
+      {
+        id: "child1",
+      },
+      [
+        React.createElement("h1", { id: "heading" }, "h1 Child1 Updated..."),
+        React.createElement("h2", { id: "heading" }, "h2 Child1 Updated..."),
+      ]
+    ),
+    React.createElement(
+      "div",
+      {
+        id: "child2",
+      },
+      [
+        React.createElement("h1", { id: "heading" }, "h1 Child2 Updated..."),
+        React.createElement("h2", { id: "heading" }, "h2 Child2 Updated..."),
+      ]
+    ),
+  ]
 );
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(parrent,heading)
+console.log(parrent);
+const heading = React.createElement(
+  "h1",
+  {
+    id: "heading",
+    className: "heading",
+  },
+  "Hello React"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parrent, heading);
